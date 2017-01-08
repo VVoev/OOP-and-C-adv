d4a68a0336b4d3c034c15030a4b71eb1c9222c77
VVoev <vladovoev@yahoo.co.uk>
2017-01-08 12:17:17 +0200

    Problem 3 Solved

---
+++ 05.Homework OOP Principles-Part 2/Problem3/TestCustomException.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Homework_OOP_Principles_Part_2.Problem3
+{
+    class TestCustomException
+    {
+        public static void Go()
+        {
+            GetValueInt();
+            GetValueDateTime();
+        }
+
+        private static void GetValueDateTime()
+        {
+            try
+            {
+                DateTime start = new DateTime(1985, 7, 17);
+                DateTime end = DateTime.Now;
+                DateTime x = new DateTime(2018, 1, 1);
+                if(x<start || x > end)
+                {
+                    throw new InvalidRangeException<DateTime>(start, end);
+                }
+            }
+            catch (InvalidRangeException<DateTime>dex)
+            {
+
+                Console.WriteLine(string.Format($"{dex.Message} ({dex.Start},{dex.End})"));
+
+            }
+        }
+
+        private static void GetValueInt()
+        {
+            try
+            {
+                int start = 1;
+                int end = 50;
+                int x = 150;
+                if(x<start || x > end)
+                {
+                    throw new InvalidRangeException<int>(start, end);
+                }
+            }
+            catch (InvalidRangeException<int>exse)
+            {
+
+                Console.WriteLine(string.Format($"{exse.Message} ({exse.Start},{exse.End})"));
+            }
+        }
+    }
+}
