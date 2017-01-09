7b7f19f3e2736b0b4a54f1d890036bb60a084367
VVoev <vladovoev@yahoo.co.uk>
2017-01-09 20:22:02 +0200

    added all item classes

---
+++ OOP Test/2. Trade and Travel_Условие на задачата/TradeAndTravel-Skeleton/TradeAndTravel-Skeleton/TradeAndTravel/ExtendedInteractionManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeAndTravel
+{
+    public class ExtendedInteractionManager : InteractionManager
+    {
+        protected override Item CreateItem(string itemTypeString, string itemNameString, Location itemLocation, Item item)
+        {
+            switch (itemTypeString)
+            {
+                case "weapon":
+                    return new Weapon(itemNameString, itemLocation);
+                case "wood":
+                    return new Wood(itemNameString, itemLocation);
+                case "iron":
+                    return new Iron(itemNameString, itemLocation);
+                default:
+                    return base.CreateItem(itemTypeString, itemNameString, itemLocation, item);
+            }
+        }
+        
+    }
+}
---
+++ OOP Test/2. Trade and Travel_Условие на задачата/TradeAndTravel-Skeleton/TradeAndTravel-Skeleton/TradeAndTravel/Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeAndTravel
+{
+    public class Weapon : Item
+    {
+        private const int InitialValue = 10;
+        public Weapon(string name,Location location = null)
+            : base(name, Weapon.InitialValue,ItemType.Weapon , location)
+        {
+        }
+    }
+}
