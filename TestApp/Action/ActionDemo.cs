fbee812370486f623d5d777198413a12d7365962
VVoev <vladovoev@yahoo.co.uk>
2017-02-04 00:48:05 +0200

    added a few more actions

--- TestApp/Action/ActionDemo.cs
+++ TestApp/Action/ActionDemo.cs
@@ -10,14 +10,21 @@ namespace TestApp.Action
     {
         public static void Start()
         {
-            var cat = new Cat(2, "Katq");
-            Action<int> multiPlyTwoAndHalf = (number) => Console.WriteLine(number*2.5);
+            Action<int, int> square = (first, second) =>
+            {
+                Console.WriteLine($"square is {Math.Sqrt(first + second)}");
+            };
+            square(4, 12);
+
+            Action<int> multiPlyTwoAndHalf = (number) => Console.WriteLine(number * 2.5);
             multiPlyTwoAndHalf(40);
+
             Action<int, string,string> introduceYourself = (age, firstname,lastname) =>
              {
                  Console.WriteLine($"I am {firstname+" "+lastname} {age} years old");
              };
             introduceYourself(31, "Vlado", "Voev");
+
 
             Action<int, int, string> Sumator = (first, second, result) =>
               {
@@ -26,6 +33,7 @@ namespace TestApp.Action
               };
             Sumator(10, 20, string.Empty);
 
+            var cat = new Cat(2, "Katq");
             Action<string, Cat> ChangeName = (name, kotka) =>
             {
                 kotka.Name = "Petya";   
@@ -40,6 +48,24 @@ namespace TestApp.Action
                 Console.WriteLine(full);
             };
             fullname("Petar", "Danov");
+
+            Action<int, int, int, int, int> BetterMathMax = (first, second, third, fourth, fifth) =>
+                {            
+                    Console.WriteLine(Math.Max(first, Math.Max(second, Math.Max(third, Math.Max(fourth, fifth)))));   
+                };
+            Console.WriteLine($"Biggest Number is ");
+            BetterMathMax(5,10,15,20,25);
+
+
+            Action<int, Cat> changeCatYears = (years, catYears) =>
+              {
+                  catYears.Age = years;
+              };
+            var myCat = new Cat(15, "Djingal");
+            changeCatYears(3, myCat);
+            Console.WriteLine($"My name is {myCat.Name} and i am {myCat.Age}years old and i am {typeof(Cat).Name}");
+
+
 
 
         }
